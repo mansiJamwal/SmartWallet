782b17e553d85e356f34168221e641c1c196f3e2
unknown <jamwalmansi16@gmail.com>
2025-07-06 14:54:35 +0530

    fixing error

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,3 @@
 export { prisma } from './client' // exports instance of prisma 
-export * from "../generated/prisma" // exports generated types from prisma
+export * from "@prisma/client" // exports generated types from prisma
 export { prisma as default } from './client';
